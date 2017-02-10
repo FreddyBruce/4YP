6a5de6fcfd5843ccb8c52ba66a9c3715c47df027
FreddyBruce <freddy.bruce@chch.ox.ac.uk>
2017-02-10 08:50:53 +0000

    updating dissimilarity

--- dissimilarity_calc.m
+++ dissimilarity_calc.m
@@ -32,16 +32,15 @@ C_null_2{1,3} = datenum(C_null_2{1,3});
 C_null_3{1,3} = datenum(C_null_3{1,3});
 
 n = 1000;
-timestep = 0;
 L = zeros(n,3);
 
 for i = 1:n
-    timestep = timestep + 60;
-    A = get_directed_adjacency(C,151,C{1,3}(21,1),timestep);
+    timestep = timestep + 300;
+    A = get_directed_adjacency(C,151,C{1,3}(1,1),timestep);
     A_null_1 = get_directed_adjacency(C_null_1,151,C_null_1{1,3}(1,1),timestep);
-    A_null_2 = get_directed_adjacency(C_null_2,151,C_null_2{1,3}(1,1),timestep);
-    A_null_3 = get_directed_adjacency(C_null_3,151,C_null_3{1,3}(1,1),timestep);
+%     A_null_2 = get_directed_adjacency(C_null_2,151,C_null_2{1,3}(1,1),timestep);
+%     A_null_3 = get_directed_adjacency(C_null_3,151,C_null_3{1,3}(1,1),timestep);
     L(i,1) = dissimilarity(A,A_null_1);
-    L(i,2) = dissimilarity(A,A_null_2);
-    L(i,3) = dissimilarity(A,A_null_3);
+%     L(i,2) = dissimilarity(A,A_null_2);
+%     L(i,3) = dissimilarity(A,A_null_3);
 end
--- randomize_emails.m
+++ randomize_emails.m
@@ -1,6 +1,6 @@
 %% Load data
 % Get path to a subfolder off the current working directory
-filePattern = fullfile(pwd, 'Data/enronmail/update');
+filePattern = fullfile(pwd, 'Data/enronmail/update/dissi_2000');
 addpath(filePattern);
 filePattern2 = fullfile(pwd, 'useful_network_tools');
 addpath(filePattern2);
@@ -14,5 +14,5 @@ dates = importdata('dates.csv',',');
 for i = 1:3
     emails_random = emails(randperm(length(emails)));
     output = [emails_random, dates];
-    cell2csv(sprintf('random_emails_%d.csv',i), output, ',');
+    cell2csv(sprintf('null_%d_2000.csv',i), output, ',');
 end
