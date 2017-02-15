424938a50925d6a9bcaa8da97daf7e0a0e8172f6
FreddyBruce <freddy.bruce@chch.ox.ac.uk>
2017-02-15 11:03:28 +0000

    altering indices

--- rolling_window_comm_det.m
+++ rolling_window_comm_det.m
@@ -3,6 +3,12 @@
 % Add cmmDetNMF folder to working directory
 filePattern3 = fullfile(pwd, 'commDetNMF');
 addpath(filePattern3);
+% Get path to a subfolder off the current working directory
+filePattern = fullfile(pwd, 'Data');
+addpath(filePattern);
+
+load('rolling_window.mat');
+
 
 step = 's1';
 
@@ -14,7 +20,7 @@ W.(step) = w;
 H.(step) = h;
 
 % Import next Adjacency Matrices and running commDetNMF
-for i = 2:11
+for i = 2:100
     % Generating structure name
     m = num2str(i);
     step = strcat('s',m);
